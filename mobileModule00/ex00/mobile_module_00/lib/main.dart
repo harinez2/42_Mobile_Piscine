ea82d8968360c4aac925a4ac0320374bc4a18c1f
harinez2 <rogistyle@gmail.com>
2024-04-06 00:58:12 +0900

    added widgets

--- mobileModule00/ex00/mobile_module_00/lib/main.dart
+++ mobileModule00/ex00/mobile_module_00/lib/main.dart
@@ -9,10 +9,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, 
+            children: [
+              Card(
+                color: theme.colorScheme.primary,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Text('A simple text', style: style),
+                )
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('Button pressed');
+                },
+                child: Text('Click me'),
+              ),
+            ],
+          ),
         ),
       ),
     );
