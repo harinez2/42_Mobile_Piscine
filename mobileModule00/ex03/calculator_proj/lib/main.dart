75169ecd0c34bf86a88f484bf0ba3d3f75416fbb
harinez2 <rogistyle@gmail.com>
2024-04-17 21:39:51 +0900

    intermediate of calculating

--- mobileModule00/ex03/calculator_proj/lib/main.dart
+++ mobileModule00/ex03/calculator_proj/lib/main.dart
@@ -8,7 +8,12 @@ void main() {
 
 class MainAppState extends ChangeNotifier {
   final numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '00', '.'];
-  final operators = ['+', '-', 'x', '/',];
+  final operators = [
+    '+',
+    '-',
+    'x',
+    '/',
+  ];
   var formula = '0';
   var answer = '0';
 
@@ -35,19 +40,75 @@ class MainAppState extends ChangeNotifier {
       // if (operators.contains(formula[formula.length - 1])) {
       //   formula = formula.substring(0, formula.length - 1) + op;
       // } else {
-        formula += op;
+      formula += op;
       // }
     } else if (op == '=') {
       // =
-      answer = Calculate.calc(formula);
+      answer = Calculate.calc(formula).toString();
     }
     notifyListeners();
   }
 }
 
 class Calculate {
-  static String calc(dynamic formula) {
-    return '1';
+  static double calc(dynamic formula) {
+    final numbers = [
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '00',
+      '.'
+    ];
+    final operators = [
+      '+',
+      '-',
+      'x',
+      '/',
+    ];
+    double leftNum = 0;
+    double rightNum = 0;
+    double multiply = 10;
+    var operator = '';
+    var i = 0;
+
+    while (i < formula.length) {
+      if (numbers.contains(formula[i])) {
+        if (formula[i] == '.') {
+          multiply = 0.1;
+        } else {
+          rightNum = rightNum * multiply + double.parse(formula[i]);
+
+          // if the end of a number
+          if (i + 1 < formula.length && operator.contains(formula[i + 1])
+              || i  + 1 == formula.length) {
+            if (operator == '+') {
+              leftNum += rightNum;
+            } else if (operator == '-') {
+              leftNum -= rightNum;
+            } else if (operator == 'x') {
+              leftNum *= rightNum;
+            } else if (operator == '/') {
+              leftNum /= rightNum;
+            }
+            rightNum = 0;
+            operator = '';
+            multiply = 10;
+          }
+        }
+      } else if (operators.contains(formula[i]) &&
+          (i + 1 < formula.length && !operators.contains(formula[i + 1]))) {
+        operator = formula[i];
+      }
+      i++;
+    }
+    return leftNum;
   }
 }
 
