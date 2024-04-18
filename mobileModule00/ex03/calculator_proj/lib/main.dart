adcf7aada58bfd6e52c05e274584ef94d371a083
harinez2 <rogistyle@gmail.com>
2024-04-18 23:24:36 +0900

    added formula analyzing func

--- mobileModule00/ex03/calculator_proj/lib/main.dart
+++ mobileModule00/ex03/calculator_proj/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screens/home_screen.dart';
@@ -8,12 +10,7 @@ void main() {
 
 class MainAppState extends ChangeNotifier {
   final numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '00', '.'];
-  final operators = [
-    '+',
-    '-',
-    'x',
-    '/',
-  ];
+  final operators = ['+', '-', 'x', '/'];
   var formula = '0';
   var answer = '0';
 
@@ -24,7 +21,9 @@ class MainAppState extends ChangeNotifier {
       answer = '0';
     } else if (op == 'C') {
       // C
-      formula = formula.substring(0, formula.length - 1);
+      if (formula.isNotEmpty) {
+        formula = formula.substring(0, formula.length - 1);
+      }
       if (formula.isEmpty) {
         formula = '0';
       }
@@ -51,64 +50,94 @@ class MainAppState extends ChangeNotifier {
 }
 
 class Calculate {
-  static double calc(dynamic formula) {
-    final numbers = [
-      '0',
-      '1',
-      '2',
-      '3',
-      '4',
-      '5',
-      '6',
-      '7',
-      '8',
-      '9',
-      '00',
-      '.'
-    ];
-    final operators = [
-      '+',
-      '-',
-      'x',
-      '/',
-    ];
-    double leftNum = 0;
-    double rightNum = 0;
+  static dynamic calc(String formula) {
+    final numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    final operators = ['+', '-', 'x', '/'];
+    List<String> operatorList = [' '];
+    List<double> numberList = [];
+    int currentIdx = 0;
     double multiply = 10;
-    var operator = '';
-    var i = 0;
 
+    // 数式を配列に格納する
+    int i = 0;
     while (i < formula.length) {
-      if (numbers.contains(formula[i])) {
+      if (operators.contains(formula[i])) {
+        // オペレータの場合：
+        // リセット
+        multiply = 10;
+        currentIdx++;
+        // 先頭に*/があるのは不正
+        if (i == 0 && (formula[i] == '*' || formula[i] == '/')) {
+          break;
+        }
+        // オペレータが連続し、2個目が*/がある場合は不正
+        if (operatorList.length == currentIdx + 1 &&
+            (formula[i] == '*' || formula[i] == '/')) {
+          break;
+        }
+        operatorList.add(formula[i]);
+      } else if (numbers.contains(formula[i])) {
+        // 数値の場合：
+        // 数字の最初の位置文字目の場合、リストに0を追加する
+        if (numberList.length == currentIdx) {
+          numberList.add(0);
+        }
         if (formula[i] == '.') {
           multiply = 0.1;
         } else {
-          rightNum = rightNum * multiply + double.parse(formula[i]);
-
-          // if the end of a number
-          if (i + 1 < formula.length && operator.contains(formula[i + 1])
-              || i  + 1 == formula.length) {
-            if (operator == '+') {
-              leftNum += rightNum;
-            } else if (operator == '-') {
-              leftNum -= rightNum;
-            } else if (operator == 'x') {
-              leftNum *= rightNum;
-            } else if (operator == '/') {
-              leftNum /= rightNum;
-            }
-            rightNum = 0;
-            operator = '';
-            multiply = 10;
-          }
+          numberList[currentIdx] =
+              numberList[currentIdx] * multiply + double.parse(formula[i]);
         }
-      } else if (operators.contains(formula[i]) &&
-          (i + 1 < formula.length && !operators.contains(formula[i + 1]))) {
-        operator = formula[i];
+        operators.add('+');
       }
       i++;
     }
-    return leftNum;
+    if (i != formula.length || operatorList.length != numberList.length) {
+      //数式不正
+      return 'Invalid formula';
+    }
+    if (true) {
+      // debug
+      print(operatorList);
+      print(numberList);
+    }
+
+    // 計算する
+    // double leftNum = 0;
+    // double rightNum = 0;
+    // var operator = '';
+    // var i = 0;
+    // while (i < formula.length) {
+    //   if (numbers.contains(formula[i])) {
+    //     if (formula[i] == '.') {
+    //       multiply = 0.1;
+    //     } else {
+    //       rightNum = rightNum * multiply + double.parse(formula[i]);
+
+    //       // if the end of a number
+    //       if (i + 1 < formula.length && operator.contains(formula[i + 1]) ||
+    //           i + 1 == formula.length) {
+    //         if (operator == '+') {
+    //           leftNum += rightNum;
+    //         } else if (operator == '-') {
+    //           leftNum -= rightNum;
+    //         } else if (operator == 'x') {
+    //           leftNum *= rightNum;
+    //         } else if (operator == '/') {
+    //           leftNum /= rightNum;
+    //         }
+    //         rightNum = 0;
+    //         operator = '';
+    //         multiply = 10;
+    //       }
+    //     }
+    //   } else if (operators.contains(formula[i]) &&
+    //       (i + 1 < formula.length && !operators.contains(formula[i + 1]))) {
+    //     operator = formula[i];
+    //   }
+    //   i++;
+    // }
+    return numberList[0];
   }
 }
 
