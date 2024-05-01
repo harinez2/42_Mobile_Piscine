ca6344ddf56dd5b80456e205d70ae279c2bbc28f
harinez2 <yonishi@student.42tokyo.jp>
2024-05-01 10:41:14 +0900

    initial commit of ex01

---
+++ Module01/ex01/weather_proj/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'screens/currently.dart';
+import 'screens/today.dart';
+import 'screens/weekly.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  MainAppState createState() => MainAppState();
+}
+
+class MainAppState extends State<MainApp> {
+  int _selectedIndex = 0;
+  final PageController _pageController = PageController(initialPage: 0);
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    CurrentlyTab(),
+    TodayTab(),
+    WeeklyTab(),
+  ];
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _pageController.animateToPage(_selectedIndex,
+          duration: const Duration(milliseconds: 200), curve: Curves.easeInOut);
+    });
+  }
+
+  void _onDestinationSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _pageController.jumpToPage(_selectedIndex);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          // 左側のアイコン
+          leading: const Icon(Icons.search),
+          // タイトルテキスト
+          title: TextField(
+            textAlign: TextAlign.left,
+            controller: TextEditingController(),
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              labelText: 'Search location...',
+            ),
+          ),
+          // 右側のアイコン一覧
+          actions: <Widget>[
+            VerticalDivider(
+              color: Colors.blueGrey.shade300,
+              thickness: 2,
+              indent: 10,
+              endIndent: 10,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.assistant_navigation),
+            ),
+          ],
+        ),
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: _onPageChanged,
+          children: _widgetOptions,
+        ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _selectedIndex,
+          onDestinationSelected: _onDestinationSelected,
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+          destinations: const <NavigationDestination>[
+            NavigationDestination(
+                icon: Icon(Icons.settings), label: 'Currently'),
+            NavigationDestination(icon: Icon(Icons.event), label: 'Today'),
+            NavigationDestination(
+                icon: Icon(Icons.date_range), label: 'Weekly'),
+          ],
+        ),
+      ),
+    );
+  }
+}
