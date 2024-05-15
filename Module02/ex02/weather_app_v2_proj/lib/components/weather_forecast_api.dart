9c1c47c688916e6e28325a6ddddb256491782d7d
harinez2 <rogistyle@gmail.com>
2024-05-15 19:23:31 +0900

    refactoring

--- Module02/ex02/weather_app_v2_proj/lib/components/weather_forecast_api.dart
+++ Module02/ex02/weather_app_v2_proj/lib/components/weather_forecast_api.dart
@@ -32,3 +32,35 @@ Future<WeatherForecast> fetchForecastCurrent(
     throw Exception('Failed to Load WeatherForecast');
   }
 }
+
+String getWeatherString(int code) {
+  if (code == 0) {
+    return 'Clear sky';
+  } else if (code == 1 || code == 2 || code == 3) {
+    return 'Mainly clear, partly cloudy, and overcast';
+  } else if (code == 45 || code == 48) {
+    return 'Fog and depositing rime fog';
+  } else if (code == 51 || code == 53 || code == 55) {
+    return 'Drizzle: Light, moderate, and dense intensity';
+  } else if (code == 56 || code == 57) {
+    return 'Freezing Drizzle: Light and dense intensity';
+  } else if (code == 61 || code == 63 || code == 65) {
+    return 'Rain: Slight, moderate and heavy intensity';
+  } else if (code == 66 || code == 67) {
+    return 'Freezing Rain: Light and heavy intensity';
+  } else if (code == 71 || code == 73 || code == 75) {
+    return 'Snow fall: Slight, moderate, and heavy intensity';
+  } else if (code == 77) {
+    return 'Snow grains';
+  } else if (code == 80 || code == 81 || code == 82) {
+    return 'Rain showers: Slight, moderate, and violent';
+  } else if (code == 85 || code == 86) {
+    return 'Snow showers slight and heavy';
+  } else if (code == 95) {
+    return 'Thunderstorm: Slight or moderate';
+  } else if (code == 96 || code == 99) {
+    return 'Thunderstorm with slight and heavy hail';
+  } else {
+    return 'Undefined';
+  }
+}
--- Module02/ex02/weather_app_v2_proj/lib/screens/currently.dart
+++ Module02/ex02/weather_app_v2_proj/lib/screens/currently.dart
@@ -23,46 +23,12 @@ class CurrentlyTabState extends State<CurrentlyTab> {
       if (widget.geoData != {}) {
         forecast = await fetchForecastCurrent(
             widget.geoData['latitude'], widget.geoData['longitude']);
-        // print(GeoLocator.getDetailText(position));
         return forecast;
       } else {
         return null;
       }
     } catch (error) {
       return null;
-      // forecast.; error
-    }
-  }
-
-  static String getWeatherString(int code) {
-    if (code == 0) {
-      return 'Clear sky';
-    } else if (code == 1 || code == 2 || code == 3) {
-      return 'Mainly clear, partly cloudy, and overcast';
-    } else if (code == 45 || code == 48) {
-      return 'Fog and depositing rime fog';
-    } else if (code == 51 || code == 53 || code == 55) {
-      return 'Drizzle: Light, moderate, and dense intensity';
-    } else if (code == 56 || code == 57) {
-      return 'Freezing Drizzle: Light and dense intensity';
-    } else if (code == 61 || code == 63 || code == 65) {
-      return 'Rain: Slight, moderate and heavy intensity';
-    } else if (code == 66 || code == 67) {
-      return 'Freezing Rain: Light and heavy intensity';
-    } else if (code == 71 || code == 73 || code == 75) {
-      return 'Snow fall: Slight, moderate, and heavy intensity';
-    } else if (code == 77) {
-      return 'Snow grains';
-    } else if (code == 80 || code == 81 || code == 82) {
-      return 'Rain showers: Slight, moderate, and violent';
-    } else if (code == 85 || code == 86) {
-      return 'Snow showers slight and heavy';
-    } else if (code == 95) {
-      return 'Thunderstorm: Slight or moderate';
-    } else if (code == 96 || code == 99) {
-      return 'Thunderstorm with slight and heavy hail';
-    } else {
-      return 'Undefined';
     }
   }
 
@@ -80,7 +46,6 @@ class CurrentlyTabState extends State<CurrentlyTab> {
             children = widget.errorText!;
             errorFlag = true;
           } else if (snapshot.hasData) {
-            print(forecast?.forecastData['current']['weather_code']);
             final weatherString = getWeatherString(
                 forecast?.forecastData['current']['weather_code']);
             children = """
