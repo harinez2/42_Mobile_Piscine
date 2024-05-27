c6f3a651c30df59a8b77eb8376cb7e3d5ecd3e8c
harinez2 <rogistyle@gmail.com>
2024-05-27 14:16:11 +0900

    bug fix of time zone

--- Module03/ex03/weather_final_proj/lib/components/weather_forecast_api.dart
+++ Module03/ex03/weather_final_proj/lib/components/weather_forecast_api.dart
@@ -26,7 +26,7 @@ class WeatherForecast {
 Future<WeatherForecast> fetchForecastCurrent(
     double latitude, double longitude) async {
   final res = await http.get(Uri.parse(
-      '$weatherForecastApiRoute?latitude=$latitude&longitude=$longitude&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min'));
+      '$weatherForecastApiRoute?latitude=$latitude&longitude=$longitude&timezone=Asia%2FTokyo&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min'));
   if (res.statusCode == 200) {
     return WeatherForecast.fromJson(jsonDecode(res.body));
   } else {
