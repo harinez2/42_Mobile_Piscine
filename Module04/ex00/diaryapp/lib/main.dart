4e32c71c5f06a7a301403cff62b98c6525aaa6eb
harinez2 <rogistyle@gmail.com>
2024-06-27 23:49:02 +0900

    firebase code

--- Module04/ex00/diaryapp/lib/main.dart
+++ Module04/ex00/diaryapp/lib/main.dart
@@ -1,11 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+
+  void listenUserState() {
+    // リスナーの登録直後。
+    // ユーザーがログインしたとき。
+    // 現在のユーザーがログアウトしたとき。
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('authStateChanges: User is currently signed out!');
+      } else {
+        print('authStateChanges: User is signed in!');
+      }
+    });
+
+    // リスナーの登録直後。
+    // ユーザーがログインしたとき。
+    // 現在のユーザーがログアウトしたとき。
+    // 現在のユーザーのトークンが変更されたとき。
+    FirebaseAuth.instance.idTokenChanges().listen((User? user) {
+      if (user == null) {
+        print('idTokenChanges: User is currently signed out!');
+      } else {
+        print('idTokenChanges: User is signed in!');
+      }
+    });
+
+    // リスナーの登録直後。
+    // ユーザーがログインしたとき。
+    // 現在のユーザーがログアウトしたとき。
+    // 現在のユーザーのトークンが変更されたとき。
+    // FirebaseAuth.instance.currentUser が提供する次のメソッドが呼び出されたとき:
+    // reload()
+    // unlink()
+    // updateEmail()
+    // updatePassword()
+    // updatePhoneNumber()
+    // updateProfile()
+    FirebaseAuth.instance.userChanges().listen((User? user) {
+      if (user == null) {
+        print('userChanges: User is currently signed out!');
+      } else {
+        print('userChanges: User is signed in!');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +69,7 @@ class MainApp extends StatelessWidget {
               const Text('Welcome to your diary'),
               const SizedBox(height: 30.0),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {listenUserState();},
                 child: const Text('Login'),
               ),
             ],
